89c57b1762237942e211383e33232890697b0fcb
Alan Pledger <alan@makeswift.com>
2021-12-31 00:18:04 -0500

    wip

---
+++ components/Button/Button.tsx
@@ -0,0 +1,43 @@
+import { ComponentPropsWithoutRef, forwardRef, Ref } from 'react'
+
+type Size = 'small' | 'medium' | 'large'
+
+type BaseProps = {
+  color?: 'white' | 'black'
+  size?: Size
+  active?: boolean
+}
+
+type Props = BaseProps & Omit<ComponentPropsWithoutRef<'button'>, keyof BaseProps>
+
+/**
+ * Primary UI component for user interaction.
+ */
+export const Button = forwardRef(function Button(
+  {
+    color = 'black',
+    size = 'medium',
+    disabled = false,
+    active = false,
+    type = 'button',
+    children,
+    ...rest
+  }: Props,
+  ref: Ref<HTMLButtonElement>,
+) {
+  return (
+    <button
+      {...rest}
+      className="cursor-click relative rounded-md flex items-center focus:outline-none
+      disabled:bg-gray-200 disabled:text-gray-600 disabled:cursor-not-allowed
+      drop-shadow-md border border-black/15
+      p-4 bg-indigo-500 text-white outline-2 hover:outline-offset-1 active:outline-offset-1 outline-black
+      disabled:hover:bg-gray-200 disabled:hover:text-gray-600 fill-current font-bold"
+      ref={ref}
+      disabled={disabled}
+      type={type}
+    >
+      {children}
+    </button>
+  )
+})
