9fbdca1a8261073b30df9cd3536a436a1dd94291
Alan Pledger <alan@makeswift.com>
2022-01-18 19:09:01 -0500

    set players

--- services/players.ts
+++ services/players.ts
@@ -3,22 +3,22 @@ import { Player } from './types'
 export const players: Player[] = [
   {
     id: '1',
-    name: 'Sean',
-    factionId: 'blue-human',
-  },
-  {
-    id: '2',
-    name: 'Dan',
-    factionId: 'white-alien',
-  },
-  {
-    id: '3',
     name: 'Alan',
     factionId: 'yellow-alien',
   },
   {
+    id: '2',
+    name: 'Brian',
+    factionId: 'white-alien',
+  },
+  {
+    id: '3',
+    name: 'Sean',
+    factionId: 'blue-alien',
+  },
+  {
     id: '4',
-    name: 'Brad',
-    factionId: 'red-human',
+    name: 'Ryan',
+    factionId: 'black-alien',
   },
 ]
