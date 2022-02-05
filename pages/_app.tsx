cb588e5d359009769451551236125d39af7d751f
Alan <alan@makeswift.com>
2022-02-05 14:15:12 -0500

    fix: add correct nextjs app types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+import { AppProps } from 'next/app'
+
 import '../styles/globals.css'
 import { GameContextProvider } from '../services/useGameContext'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GameContextProvider>
       <Component {...pageProps} />
