4a298808f1afa5ea0301103509bf331bd172b202
Alan <alan@makeswift.com>
2022-02-06 18:39:17 -0500

    Add responsive warning

--- pages/setup.tsx
+++ pages/setup.tsx
@@ -59,32 +59,35 @@ export default function Setup() {
   ])
 
   return (
-    <div className="flex flex-col h-screen">
-      <div className="flex h-20 p-4 items-center justify-between">
-        <StatBadge label="Game setup" />
-        <div className="flex gap-4">
-          <IconButton
-            disabled={!canUndo}
-            onClick={() => {
-              dispatch({ type: 'UNDO' })
-            }}
-          >
-            <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
-          </IconButton>
-          <IconButton
-            disabled={!canRedo}
-            onClick={() => {
-              dispatch({ type: 'REDO' })
-            }}
-          >
-            <FontAwesomeIcon icon={faArrowAltCircleRight} size="2x" />
-          </IconButton>
+    <>
+      <div className="hidden lg:flex flex-col h-screen">
+        <div className="flex h-20 p-4 items-center justify-between">
+          <StatBadge label="Game setup" />
+          <div className="flex gap-4">
+            <IconButton
+              disabled={!canUndo}
+              onClick={() => {
+                dispatch({ type: 'UNDO' })
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowAltCircleLeft} size="2x" />
+            </IconButton>
+            <IconButton
+              disabled={!canRedo}
+              onClick={() => {
+                dispatch({ type: 'REDO' })
+              }}
+            >
+              <FontAwesomeIcon icon={faArrowAltCircleRight} size="2x" />
+            </IconButton>
+          </div>
         </div>
-      </div>
-      <div className="flex-grow flex items-center">
-        <form className="flex flex-col gap-10 max-w-5xl m-auto w-full" onSubmit={handleSubmit}>
-          <div className="p-4 overflow-x-auto">
-            <div className="flex flex-wrap gap-4" style={{ width: 1000 }}>
+        <div className="flex-grow flex items-center">
+          <form
+            className="flex flex-col gap-10 max-w-5xl m-auto w-full items-center"
+            onSubmit={handleSubmit}
+          >
+            <div className="grid grid-cols-6 gap-4">
               {errors.factionId && touched.factionId && (
                 <div className="absolute bottom-full mb-4 text-lg text-red-500 uppercase justify-center w-full flex">
                   Please select a faction
@@ -119,53 +122,58 @@ export default function Setup() {
                 )
               })}
             </div>
-          </div>
 
-          <div className="grid gap-1 w-96 m-auto">
-            <div className="text-md uppercase text-gray-500">Name</div>
-            <div className="flex gap-2 relative">
-              <Input
-                ref={nameInput}
-                className="flex-grow"
-                value={name}
-                onChange={({ currentTarget }) => setFieldValue('name', currentTarget.value)}
-              />
-              <Button className="flex-shrink-0" color="black" type="submit">
-                Add Player
-              </Button>
-              {errors.name && touched.name && (
-                <div className="absolute top-full mt-2 text-md text-red-500 uppercase">
-                  Name is required
-                </div>
-              )}
+            <div className="grid gap-1 w-96 m-auto">
+              <div className="text-md uppercase text-gray-500">Name</div>
+              <div className="flex gap-2 relative">
+                <Input
+                  ref={nameInput}
+                  className="flex-grow"
+                  value={name}
+                  onChange={({ currentTarget }) => setFieldValue('name', currentTarget.value)}
+                />
+                <Button className="flex-shrink-0" color="black" type="submit">
+                  Add Player
+                </Button>
+                {errors.name && touched.name && (
+                  <div className="absolute top-full mt-2 text-md text-red-500 uppercase">
+                    Name is required
+                  </div>
+                )}
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div className="bg-black h-20 flex items-center justify-between px-4 gap-2 min-w-0">
+          <div className="flex gap-4">
+            <StatBadge label="Players" />
+            <div className="flex gap-2">
+              {orderedPlayers.map(player => (
+                <FactionBadge
+                  key={player.id}
+                  className="cursor-default"
+                  size="small"
+                  factionId={player.factionId}
+                />
+              ))}
             </div>
           </div>
-        </form>
-      </div>
-
-      <div className="bg-black h-20 flex items-center justify-between px-4 gap-2 min-w-0">
-        <div className="flex gap-4 overflow-x-auto">
-          <StatBadge label="Players" className="hidden sm:flex" />
-          <div className="flex gap-2">
-            {orderedPlayers.map(player => (
-              <FactionBadge
-                key={player.id}
-                className="cursor-default"
-                size="small"
-                factionId={player.factionId}
-              />
-            ))}
-          </div>
+          <Button
+            className="flex-shrink-0"
+            color="white"
+            disabled={players.length < 2}
+            onClick={() => router.push('/play')}
+          >
+            Create game
+          </Button>
         </div>
-        <Button
-          className="flex-shrink-0"
-          color="white"
-          disabled={players.length < 2}
-          onClick={() => router.push('/play')}
-        >
-          Create game
-        </Button>
       </div>
-    </div>
+      <div className="flex lg:hidden flex-col h-screen items-center justify-center p-10">
+        <div className="max-w-md text-3xl text-center uppercase">
+          Tactical Bankruptcy is best experienced with a keyboard
+        </div>
+      </div>
+    </>
   )
 }
